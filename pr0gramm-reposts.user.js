4231b6453dde716db226da9a822d4571d3312d42
Mopsalarm <mopsalarm@users.noreply.github.com>
2017-11-18 19:03:47 +0100

    Make it work with greasemonkey 4

--- pr0gramm-reposts.user.js
+++ pr0gramm-reposts.user.js
@@ -1,41 +1,88 @@
 // ==UserScript==
 // @name         pr0gramm-reposts
 // @namespace    http://pr0gramm.com/user/Mopsalarm
-// @version      1.0.0
+// @version      1.1.0
 // @description  Adds little "repost" markers to the stream
 // @author       Mopsalarm
 // @match        http://pr0gramm.com/*
 // @match        https://pr0gramm.com/*
-// @require      https://raw.githubusercontent.com/rapportive-oss/jquery-parsequery/master/jquery.parsequery.js
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
 
-(function() {
+window.eval(`(function() {
   "use strict";
 
-  jQuery("<style>").appendTo("body").text('\
-    @import url(https://fonts.googleapis.com/css?family=Droid+Sans:700); \
-    .thumb { \
-      position: relative; \
-    } \
-    .thumb__repost { \
-      position: absolute; \
-      display: none; \
-      top: 0; left: 0; \
-      line-height: 128px; \
-      color: white; \
-      font-size: 1.2em; \
-      font-weight: bold; \
-      text-align: center; \
-      background: rgba(0, 0, 0, 0.7); \
-      width: 100%; \
-      transition: background 0.5s; \
-      font-family: "Droid Sans"; \
-    } \
-    .thumb__repost:hover { \
-      background: rgba(0, 0, 0, 0.2); \
-    }')
+  /**
+   * Copyright (c) 2010 Conrad Irwin <conrad@rapportive.com> MIT license.
+   * Based loosely on original: Copyright (c) 2008 mkmanning MIT license.
+   *
+   * Parses CGI query strings into javascript objects.
+   *
+   * See the README for details.
+   **/
+  (function ($) {
+      $.parseQuery = function (options) {
+
+          var config = {query: window.location.search || ""},
+              params = {};
+
+          if (typeof options === 'string') {
+              options = {query: options};
+          }
+          $.extend(config, $.parseQuery, options);
+          config.query = config.query.replace(/^\\?/, '');
+
+          if (config.query.length > 0) {
+              $.each(config.query.split(config.separator), function (i, param) {
+                  var pair = param.split('='),
+                      key = config.decode(pair.shift(), null).toString(),
+                      value = config.decode(pair.length ? pair.join('=') : null, key);
+
+                  if (config.array_keys.test ? config.array_keys.test(key) : config.array_keys(key)) {
+                      params[key] = params[key] || [];
+                      params[key].push(value);
+                  } else {
+                      params[key] = value;
+                  }
+              });
+          }
+          return params;
+      };
+      $.parseQuery.decode = $.parseQuery.default_decode = function (string) {
+          return decodeURIComponent((string || "").replace(/\\+/g, ' '));
+      };
+      $.parseQuery.array_keys = function () {
+          return false;
+      };
+      $.parseQuery.separator = "&";
+  }(window.jQuery || window.Zepto));
+
+
+
+
+  jQuery("<style>").appendTo("body").text(\`
+    @import url(https://fonts.googleapis.com/css?family=Droid+Sans:700);
+    .thumb {
+      position: relative;
+    }
+    .thumb__repost {
+      position: absolute;
+      display: none;
+      top: 0; left: 0;
+      line-height: 128px;
+      color: white;
+      font-size: 1.2em;
+      font-weight: bold;
+      text-align: center;
+      background: rgba(0, 0, 0, 0.7);
+      width: 100%;
+      transition: background 0.5s;
+      font-family: "Droid Sans", sans-serif;
+    }
+    .thumb__repost:hover {
+      background: rgba(0, 0, 0, 0.2);
+    }\`)
 
   var $style = jQuery("<style>").appendTo("body");
 
@@ -101,4 +148,4 @@
   // maybe some items already exist, update those once
   jQuery(".thumb:not(.thumb--repost-enhanced)").append('<div class="thumb__repost">repost</div>');
 
-})();
+})();`);
